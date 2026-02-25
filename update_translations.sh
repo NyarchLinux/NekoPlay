85e5fb172daa65aea5fbfa8fe20f1047bfaa251e
Mimolet <themimolet@proton.me>
2026-02-25 21:12:19 +0100

    fix: got update_translations script working again

--- update_translations.sh
+++ update_translations.sh
@@ -10,23 +10,24 @@ grep '\.blp$' po/POTFILES.in > /tmp/POTFILES_CINE.blp
 
 # create pot
 xgettext --files-from=/tmp/POTFILES_CINE \
-         --output="$OUTPUT" --package-name="$PACKAGE_NAME" \
-         --from-code="$ENCODING" --add-comments \
-         --keyword=_ --keyword=C_:1c,2
+	--output="$OUTPUT" --package-name="$PACKAGE_NAME" \
+	--from-code="$ENCODING" --add-comments \
+	--keyword=_ --keyword=C_:1c,2
 
 # join pot
 xgettext --files-from=/tmp/POTFILES_CINE.blp \
-         --output="$OUTPUT" --package-name="$PACKAGE_NAME" \
-         --from-code="$ENCODING" --add-comments \
-         --keyword=_ --keyword=C_:1c,2 \
-         $LANGUAGE_BLP \
-         --join-existing
+	--output="$OUTPUT" --package-name="$PACKAGE_NAME" \
+	--from-code="$ENCODING" --add-comments \
+	--keyword=_ --keyword=C_:1c,2 \
+	$LANGUAGE_BLP \
+	--join-existing
 
 
 rm /tmp/POTFILES_CINE /tmp/POTFILES_CINE.blp
 
 sed -i 's/charset=CHARSET/charset=UTF-8/g' $OUTPUT
 
-grep '^' "$LINGUAS_FILE" | while read -r lang_file; do
-    msgmerge --previous --backup=none --update "po/${lang_file}.po" "$OUTPUT"
+grep -v '^#' "$LINGUAS_FILE" | while read -r lang_file; do
+    msgmerge --previous --backup=none --update "po/${lang_file}.po" "$OUTPUT" \
+    || echo "Error with : $lang_file" >&2
 done
